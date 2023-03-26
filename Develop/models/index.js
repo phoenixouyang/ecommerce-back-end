bad2ef9d68785e1a5665fde4c8b6f3a4270d87fc
Pholobo <ouyangphoenix@gmail.com>
2023-03-25 22:15:52 -0400

    fixes belongsToMany associations

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -17,12 +17,16 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
+  through: 'ProductTag',
+  foreignKey: 'product_id',
+  as: 'tags'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
+  through: 'ProductTag',
+  foreignKey: 'tag_id',
+  as: 'products'
 });
 
 module.exports = {
